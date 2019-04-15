900b02731f63f5b1a03177a890e0d8efc14df75c
Jonathan Butler <Adroitboss@gmail.com>
2019-04-15 13:00:03 -0400

    Add files via upload
    Added Voice Recognition

---
+++ Mail Template Generator/Mail Template Generator/NewUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mail_Template_Generator
+{
+    public partial class NewUser : Form
+    {
+        private Form1 Sender;
+
+        public NewUser(Form1 form1)
+        {
+            InitializeComponent();
+            Sender = form1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Sender.add_To_CSV(textBox1.Text, textBox2.Text);
+            this.Close();
+        }
+    }
+}
